4764a42e469c0b47bf19e81a4ba7b52b2a9d9776
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-10-05 09:02:43 +0200

    Implemented authorized demo controller

--- src/main/java/org/bash/jwtsecurity/controllers/DemoController.java
+++ src/main/java/org/bash/jwtsecurity/controllers/DemoController.java
@@ -1,4 +1,20 @@
 package org.bash.jwtsecurity.controllers;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+@RequiredArgsConstructor
 public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> greet() {
+        return ResponseEntity.ok("Hello World");
+    }
+
 }
